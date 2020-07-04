25a64889f0aed575ea0a8ba96fa6f4a50f63fa5f
Mahmoud Muhammed <mandoooman@gmail.com>
2020-07-05 00:58:10 +0200

    added Swal.fire to show alert message

--- public/js/main.js
+++ public/js/main.js
@@ -8,24 +8,35 @@
 
 $(".addToCart").on("click", function () {
 
-    var button     = this , 
-        form       = this.form,
-        formdata   = new FormData(form);
+    var button = this,
+        form = this.form,
+        formdata = new FormData(form);
     $.ajax({
-        url  : form.action,
-        type : "POST",
-        data : formdata,
+        url: form.action,
+        type: "POST",
+        data: formdata,
         contentType: false,
         cache: false,
         processData: false,
         success: function (data) {
-         alert(data)
 
-        }, error : function(reject){
+            if (data.req == "success") {
+                
+                Swal.fire({
+                    position: 'top-end',
+                    icon: data.req,
+                    title: data.msg,
+                    showConfirmButton: false,
+                    timer: 1500
+                })
+            }
 
-            var response = $.parseJSON(reject.responseText) ;
+        },
+        error: function (reject) {
 
-            $.each (response.errors , function (key , val){
+            var response = $.parseJSON(reject.responseText);
+
+            $.each(response.errors, function (key, val) {
 
                 //$('#' + key + '_error').text(val[0]);
             })
